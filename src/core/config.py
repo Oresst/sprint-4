b88c6bb9f900a9fcde233c83f89f054249b30f55
Maxim <maxim.panasencko@gmail.com>
2023-10-22 09:28:38 +0300

    feat: Описан каркас приложения

---
+++ src/core/config.py
@@ -0,0 +1,28 @@
+from pydantic_settings import BaseSettings
+from pydantic import Field
+
+import os
+from logging import config as logging_config
+
+from core.logger import LOGGING
+
+# Применяем настройки логирования
+logging_config.dictConfig(LOGGING)
+
+
+class AppSettings(BaseSettings):
+    project_name: str = Field(default="movies", validation_alias="PROJECT_NAME")
+    redis_host: str = Field(default="127.0.0.1", validation_alias="REDIS_HOST")
+    redis_port: int = Field(default=6379, validation_alias="REDIS_PORT")
+    elastic_host: str = Field(default="127.0.0.1", validation_alias="ELASTIC_HOST")
+    elastic_port: int = Field(default=9200, validation_alias="ELASTIC_PORT")
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+# Корень проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app_settings = AppSettings()
--- src/main.py
+++ src/main.py
@@ -1,15 +1,44 @@
-from typing import Union
+import logging
 
+import uvicorn
+from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from redis.asyncio import Redis
 
-app = FastAPI()
+from api.v1 import films
+from core.config import app_settings
+from core.logger import LOGGING
+from db import elastic
+from db import redis
+
+app = FastAPI(
+    title=app_settings.project_name,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.on_event('startup')
+async def startup():
+    redis.redis = Redis(host=app_settings.redis_host, port=app_settings.redis_port)
+    elastic.es = AsyncElasticsearch(hosts=[f'{app_settings.elastic_host}:{app_settings.elastic_port}'])
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event('shutdown')
+async def shutdown():
+    await redis.redis.close()
+    await elastic.es.close()
+
+
+app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
