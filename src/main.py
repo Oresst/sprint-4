a90cbceabedee43396f6376308ce297a765cde5a
alex <lehater@mail.ru>
2023-10-29 13:40:09 +0300

    feat: gunicorn configured

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import logging
 
-import uvicorn
 from contextlib import asynccontextmanager
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
+from gunicorn.app.base import BaseApplication
 
 from api.v1 import films, persons, genres
 from core.config import app_settings
@@ -35,5 +35,33 @@ app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
 app.include_router(genres.router, prefix="/api/v1/genres", tags=["genres"])
 app.include_router(persons.router, prefix="/api/v1/persons", tags=["persons"])
 
+
+class StandaloneApplication(BaseApplication):
+    """Our Gunicorn application."""
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+options = {
+    "bind": "0.0.0.0",
+    "accesslog": "-",
+    "errorlog": "-",
+    "worker_class": "uvicorn.workers.UvicornWorker",
+}
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config=LOGGING, log_level=logging.DEBUG)
+    StandaloneApplication(app, options).run()
